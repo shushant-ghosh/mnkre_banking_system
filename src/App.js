c6a5530c432d6f839ec85a3ee50ccac34689f61e
shushant-ghosh <shushant.ghosh.857@gmail.com>
2024-08-29 03:29:26 +0530

    UI for Login and SignUp along with state handling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./components.scss";
+import Login from "./Components/Login/Login";
+import Signup from "./Components/SignUp/Signup";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,184 @@
+import React, { useEffect, useState } from "react";
+import "../../components.scss";
+import { message } from "antd";
+
+function Login() {
+  const [activeTab, setActiveTab] = useState("Login");
+  const [loginDetails, setLoginDetails] = useState({});
+  const [signUpDetails, setSignUpDetails] = useState({});
+
+  const handlePills = (type) => {
+    if (type === "Login") {
+      setActiveTab("Login");
+    } else {
+      setActiveTab("SignUp");
+    }
+    setLoginDetails({});
+    setSignUpDetails({});
+  };
+
+  const submitForm = (e, type) => {
+    e.preventDefault();
+    console.log(type);
+    console.log({ loginDetails, signUpDetails });
+    if (type === "Login") {
+      // Redirect to Dashboard
+    } else {
+      // Redirect to Login details
+      if (signUpDetails.password == signUpDetails.confirmPassword) {
+        message.success("User Registered Successfully");
+        setTimeout(() => {
+          handlePills("Login");
+        }, 1000);
+      } else {
+        message.error("Password does not match !");
+      }
+    }
+  };
+
+  return (
+    <div id="login" className="fluid-container">
+      <div className="Card">
+        <div id="pillSelection" className="d-flex justify-content-center">
+          <ul class="nav nav-pills">
+            <li class="nav-item">
+              <a
+                class={`nav-link ${activeTab === "Login" ? "active" : ""}`}
+                onClick={() => handlePills("Login")}
+              >
+                Login
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                class={`nav-link ${activeTab === "SignUp" ? "active" : ""}`}
+                onClick={() => handlePills("SignUp")}
+              >
+                SignUp
+              </a>
+            </li>
+          </ul>
+        </div>
+        {activeTab === "Login" && (
+          <div id="LoginForm">
+            <form>
+              <div class="form-group py-2">
+                <label for="exampleInputEmail1" className="text-white fw-bold">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Enter Username"
+                  onChange={(e) => {
+                    setLoginDetails({
+                      ...loginDetails,
+                      username: e.target.value,
+                    });
+                  }}
+                />
+              </div>
+              <div class="form-group py-2">
+                <label
+                  for="exampleInputPassword1"
+                  className="text-white fw-bold"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  class="form-control"
+                  placeholder="Password"
+                  onChange={(e) => {
+                    setLoginDetails({
+                      ...loginDetails,
+                      password: e.target.value,
+                    });
+                  }}
+                />
+              </div>
+              <div className="d-flex justify-content-center pt-4">
+                <button
+                  class="btn btn-primary LoginButton"
+                  onClick={(e) => submitForm(e, "Login")}
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
+        {activeTab === "SignUp" && (
+          <div id="SignUpForm">
+            <form>
+              <div class="form-group py-2">
+                <label for="exampleInputEmail1" className="text-white fw-bold">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Create Username"
+                  onChange={(e) => {
+                    setSignUpDetails({
+                      ...signUpDetails,
+                      username: e.target.value,
+                    });
+                  }}
+                />
+              </div>
+              <div class="form-group py-2">
+                <label
+                  for="exampleInputPassword1"
+                  className="text-white fw-bold"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  class="form-control"
+                  placeholder="Password"
+                  onChange={(e) => {
+                    setSignUpDetails({
+                      ...signUpDetails,
+                      password: e.target.value,
+                    });
+                  }}
+                />
+              </div>
+              <div class="form-group py-2">
+                <label
+                  for="exampleInputPassword1"
+                  className="text-white fw-bold"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  class="form-control"
+                  placeholder="Confirm Password"
+                  onChange={(e) => {
+                    setSignUpDetails({
+                      ...signUpDetails,
+                      confirmPassword: e.target.value,
+                    });
+                  }}
+                />
+              </div>
+              <div className="d-flex justify-content-center pt-4">
+                <button
+                  class="btn btn-primary LoginButton"
+                  onClick={(e) => submitForm(e, "SignUp")}
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Login;
